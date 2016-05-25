8074809bc95b40dad09b1e969ece281841ad8aff
Peter Stromhaug <pas282@cornell.edu>
2016-05-25 02:00:24 +0200

    seperate out main function

--- examples/stochastic_depth/debug.cpp
+++ examples/stochastic_depth/debug.cpp
@@ -18,27 +18,27 @@ using namespace std;
 
 void standardResLayer(int & elts, int & idx, vector<int>* layers_chosen, double ran, double prob);
 void transitionResLayer(int & elts, int & idx, vector<int>* layers_chosen, double ran, double prob);
-
-int main(int argc, char** argv)
-{
-	Caffe::set_mode(Caffe::GPU);
-	
-	string param_file = "examples/stochastic_depth/solver.prototxt";
-  	SolverParameter param;
-  	ReadSolverParamsFromTextFileOrDie(param_file, &param);
-  	Solver<float>* solver = SolverRegistry<float>::CreateSolver(param);	
-	shared_ptr<Net<float> > net = solver->net();
-	vector<shared_ptr<Layer<float> > > layers = net->layers();
-
-
-	vector<int>* layers_chosen = new vector<int>();
-	net->ChooseLayers_StochDep(layers_chosen);
-
-//	for (int i = 0; i < layers_chosen->size(); i++) {
-//		cout << (*layers_chosen)[i] << ": " <<layers[(*layers_chosen)[i]]->type() << endl;
-//	}
-	solver->Solve_StochDep();
-}
+//
+//int main(int argc, char** argv)
+//{
+//	Caffe::set_mode(Caffe::GPU);
+//	
+//	string param_file = "examples/stochastic_depth/solver.prototxt";
+//  	SolverParameter param;
+//  	ReadSolverParamsFromTextFileOrDie(param_file, &param);
+//  	Solver<float>* solver = SolverRegistry<float>::CreateSolver(param);
+//	shared_ptr<Net<float> > net = solver->net();
+//	vector<shared_ptr<Layer<float> > > layers = net->layers();
+//
+//
+//	vector<int>* layers_chosen = new vector<int>();
+//	net->ChooseLayers_StochDep(layers_chosen);
+//
+////	for (int i = 0; i < layers_chosen->size(); i++) {
+////		cout << (*layers_chosen)[i] << ": " <<layers[(*layers_chosen)[i]]->type() << endl;
+////	}
+//	solver->Solve_StochDep();
+//}
 
 template <typename Dtype>
 void Net<Dtype>::standardResLayer(int & elts, int & idx, vector<int>* layers_chosen, double ran, double prob) {
