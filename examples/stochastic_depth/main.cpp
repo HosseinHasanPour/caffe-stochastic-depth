37f033526bdc8c5de649f8c72873ac351bea6206
Peter Stromhaug <pas282@cornell.edu>
2016-05-25 19:00:23 +0200

    learnable_params_stochdept_

--- examples/stochastic_depth/main.cpp
+++ examples/stochastic_depth/main.cpp
@@ -291,10 +291,10 @@ void Solver<Dtype>::Step_StochDep(int iters, vector<int>* layers_chosen) {
 
         for (int i = 0; i < param_.iter_size(); ++i) {
             net_->ChooseLayers_StochDep(layers_chosen);
-            net_->SetLearnableParams_StochDep(layers_chosen);
-            for (int k = 0; k < net_->learnable_params_stochdept().size(); k++) {
-                cout << net_->learnable_params_stochdept()[k] << endl;
-            }
+//            net_->SetLearnableParams_StochDep(layers_chosen);
+//            for (int k = 0; k < net_->learnable_params_stochdept().size(); k++) {
+//                cout << net_->learnable_params_stochdept()[k] << endl;
+//            }
             loss += net_->ForwardBackward_StochDep(layers_chosen);
         }
         loss /= param_.iter_size();
