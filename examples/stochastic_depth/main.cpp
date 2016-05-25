777250ff927018d3359932d91f08e302b39c80db
Peter Stromhaug <pas282@cornell.edu>
2016-05-25 05:38:41 +0200

    added layer_num_to_learnable_params

--- examples/stochastic_depth/main.cpp
+++ examples/stochastic_depth/main.cpp
@@ -38,7 +38,7 @@ int main(int argc, char** argv)
         int layer_id = (*layers_chosen)[i];
         int mapvecsize = 0;
         if (net->layer_num_to_learnable_params().count(layer_id) > 0) {
-            typedef typename map<int, vector*>::const_iterator iter;
+            typedef typename map<int, vector<void*>*>::const_iterator iter;
             iter pair;
             pair = net->layer_num_to_learnable_params().find(layer_id);
             mapvecsize = (int)pair->second->size();
