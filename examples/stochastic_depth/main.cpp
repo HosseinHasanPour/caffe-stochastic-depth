92801cbbbde2b95a93c507276b0830c6b3c66db3
Peter Stromhaug <pas282@cornell.edu>
2016-05-26 04:10:00 +0200

    finished scaling

--- examples/stochastic_depth/main.cpp
+++ examples/stochastic_depth/main.cpp
@@ -294,11 +294,13 @@ const Dtype Net<Dtype>::ForwardFromTo_StochDep_Test(int start, int end) {
         loss += layer_loss;
         vector<Blob<Dtype>*> top_vec = top_vecs_[i];
         double prob = test_scaling_stochdept_[i];
-        for (int j = 0; j < top_vec.size(); j++) {
-            Blob<Dtype>* top_blob = top_vec[j];
-//            cout <<"i: " << i << "\t j: " << j << '\t' << layers_[i]->type() << "\t before: " << top_blob->asum_data() << "\t after: ";
-            top_blob->scale_data(prob);
-//            cout << top_blob->asum_data() << endl;
+        if (prob < 1.0) {
+            for (int j = 0; j < top_vec.size(); j++) {
+                Blob<Dtype> *top_blob = top_vec[j];
+                            cout <<"i: " << i << "\t j: " << j << '\t' << layers_[i]->type() << "\t before: " << top_blob->asum_data() << "\t after: ";
+                top_blob->scale_data(prob);
+                            cout << top_blob->asum_data() << endl;
+            }
         }
         if (debug_info_) { ForwardDebugInfo(i); }
     }
